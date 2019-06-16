9ea51c7e3f807eaf974b483c457bb19acc90e350
ArthurStocker <arthur@stocker-schmid.ch>
2019-06-16 17:54:54 +0200

    custom input in table PoC

--- components/ConditionalTable.vue
+++ components/ConditionalTable.vue
@@ -1,8 +1,10 @@
 <template>
   <b-table striped hover :name="name" :fields="definition" :items="records" :filter="filter" selectable select-mode="single" selectVariant="info" @row-selected="select">
     <template v-for="key in definition.map((field) => field.key)" :slot="key" slot-scope="records">
-      <input class="form-control" v-if="editable" v-bind:key="key" :data-dataset="name" :data-row="records.index" :data-col="key" :type="records.field.type" :value="valueTransformer('text', key, records)" :checked="valueTransformer('checkbox', key, records)" @input="storeCommitEvent" />
-      <span v-else v-bind:key="key">{{ records.item[key] }}</span>
+      <input v-if="editable" v-bind:key="key" :data-dataset="name" :data-row="records.index" :data-col="key" :type="records.field.type" :value="valueTransformer('text', key, records)" :checked="valueTransformer('checkbox', key, records)" @input="storeCommitEvent" @dblclick="storeCommitX('editable')" class="form-control" />
+      <i v-else-if="!editable && ['radio', 'checkbox'].indexOf(records.field.type) != -1 && valueTransformer('checkbox', key, records)" v-bind:key="key" class="fas fa-toggle-on"></i>
+      <i v-else-if="!editable && ['radio', 'checkbox'].indexOf(records.field.type) != -1 && !valueTransformer('checkbox', key, records)" v-bind:key="key" class="fas fa-toggle-off"></i>
+      <span v-else v-bind:key="key" @dblclick="storeCommitX('editable')">{{ records.item[key] }}</span>
     </template>
   </b-table>
 </template>
@@ -20,6 +22,12 @@ module.exports = {
     },
     storeCommitEvent(event) {
       this.$store.commit('updateRecord', { datasetname: event.target.dataset.dataset, row: event.target.dataset.row, col: event.target.dataset.col, value: (['radio', 'checkbox'].indexOf(event.target.type) != -1 ? event.target.checked : event.target.value) })
+    },
+    storeCommitX(key, value) {
+      this.$store.commit(key, !this.$store.state.editable)
+    },
+    storeCommit(key, value) {
+      this.$store.commit(key, value)
     },
     select(data) {
       var self = this
